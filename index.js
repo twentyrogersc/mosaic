a547cca161da166df26a5854e4ddd972702d7682
Craig Rogers <20rogersc@gmail.com>
2013-01-23 12:39:43 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('teller')
+var helpers = require('./lib/helpers')
+var moasic = require('./lib/moasic')
+var path = require('path')
+var server = require('./lib/server')
+
+
+// config
+
+var config = require('./config')
+config.dims = helpers.dims(config.dims)
+config.dir = path.join(__dirname, config.dir || 'images')
+config.grid = helpers.dims(config.grid)
+config.save = path.join(__dirname, config.save || 'public')
+config.cwd = __dirname
+
+
+// go
+
+moasic.make(config)
+server.go(config, 3000)
