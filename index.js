7b0e182a16016e619f4181a8c821d4d0f2ab8592
Craig Rogers <20rogersc@gmail.com>
2013-01-28 12:08:37 +0000

    Allow video as an source/output.

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+var config = require('./config')
 var helpers = require('./lib/helpers')
 var moasic = require('./lib/moasic')
 var path = require('path')
+var video = require('./lib/video')
 
 
 // config
 
-var config = require('./config')
+var videoExts = new RegExp('.(mov|mp4|avi)$')
+var isVideo = videoExts.test(config.src)
+var isUrl = helpers.isUrl(config.src)
+var ext = isVideo ? 'mp4' : 'jpg'
+
 config.dims = helpers.dims(config.dims)
-config.dir = path.join(__dirname, config.dir || 'images')
 config.grid = helpers.dims(config.grid)
-config.save = path.join(__dirname, config.save || 'moasic.jpg')
-config.tint = config.tint || 0.7
+config.dir = path.join(__dirname, config.dir || 'images')
+config.fps = config.fps ? parseInt(config.fps) : 6
+config.save = path.join(__dirname, config.save || 'moasic.'+ext)
+config.src = isUrl ? config.src : path.join(__dirname, config.src)
+config.tint = config.tint ? parseFloat(config.tint) : 0.7
 config.tmp = path.join(__dirname, config.tmp || 'tmp')
 
 
-// go
+// use moasic/video module depending on config.src
 
-moasic.make(config)
+if (isVideo) video.make(config)
+else moasic.make(config)
--- readme.md
+++ readme.md
@@ -6,10 +6,11 @@ Image moasic generator in [node.js](http://nodejs.org).
 ### required
 **dims**: dimensions in pixels of each tile in the format ```"#x#"```.  
 **grid**: number of pixels in the format ```"#x#"```.  
-**img**: url or path to source image.  
+**src**: url or path to source image or video.  
 
 ### optional
 **dir**: relative path of the directory containing images to use in the mosaic. Default is ```"images"```.  
+**fps**: number of frames per second if the src is video. Default is ```6```.  
 **save**: filename of final output. Default is ```"mosaic.jpg"```.  
 **tint**: opacity of image on top of original pixel colour. Default is ```0.7```.  
 **tmp**: relative path of temp directory, will be removed after process. Default is ```"tmp"```.
